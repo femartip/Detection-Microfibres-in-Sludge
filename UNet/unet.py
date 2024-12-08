0ecfe1fde32edac622a3e5f7c418c5527a18d570
femartip <felix.marti99@gmail.com>
2024-12-08 11:40:32 +0100

    Enhance UNet architecture: add additional downsampling and upsampling layers, update bottleneck dimensions, and adjust input padding for improved feature extraction

--- UNet/unet.py
+++ UNet/unet.py
@@ -90,32 +90,36 @@ class UNet(nn.Module):
         self.down_convolution_2 = DownSample(64, 128)
         self.down_convolution_3 = DownSample(128, 256)
         self.down_convolution_4 = DownSample(256, 512)
+        self.down_convolution_5 = DownSample(512, 1024)
 
-        self.bottle_neck = DoubleConv(512, 1024)
+        self.bottle_neck = DoubleConv(1024, 2048)
 
-        self.up_convolution_1 = UpSample(1024, 512)
-        self.up_convolution_2 = UpSample(512, 256)
-        self.up_convolution_3 = UpSample(256, 128)
-        self.up_convolution_4 = UpSample(128, 64)
+        self.up_convolution_1 = UpSample(2048, 1024)
+        self.up_convolution_2 = UpSample(1024, 512)
+        self.up_convolution_3 = UpSample(512, 256)
+        self.up_convolution_4 = UpSample(256, 128)
+        self.up_convolution_5 = UpSample(128, 64)
 
         self.out = nn.Conv2d(in_channels=64, out_channels=num_classes, kernel_size=1)
 
     def forward(self, x):
-        # Pad to make sure the input size is divisible by 16
-        x, pads = pad_to(x, 16)
+        # Pad to make sure the input size is divisible by 32
+        x, pads = pad_to(x, 32)
 
         down_1, p1 = self.down_convolution_1(x)
         down_2, p2 = self.down_convolution_2(p1)
         down_3, p3 = self.down_convolution_3(p2)
         down_4, p4 = self.down_convolution_4(p3)
+        down_5, p5 = self.down_convolution_5(p4)
 
-        b = self.bottle_neck(p4)
+        b = self.bottle_neck(p5)
 
-        up_1 = self.up_convolution_1(b, down_4)
-        up_2 = self.up_convolution_2(up_1, down_3)
-        up_3 = self.up_convolution_3(up_2, down_2)
-        up_4 = self.up_convolution_4(up_3, down_1)
-        out = self.out(up_4)
+        up_1 = self.up_convolution_1(b, down_5)
+        up_2 = self.up_convolution_2(up_1, down_4)
+        up_3 = self.up_convolution_3(up_2, down_3)
+        up_4 = self.up_convolution_4(up_3, down_2)
+        up_5 = self.up_convolution_5(up_4, down_1)
+        out = self.out(up_5)
         
         out = unpad(out, pads)
         return out
